a7deb90dd79e6d36d52ce6ebc62352d6950f867f
CristianD <crisdic5@gmail.com>
2022-12-11 20:41:28 -0500

    Actualizacion index

--- proyecto/views.py
+++ proyecto/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, redirect
+from django.core.paginator import Paginator
 from django.views.generic import CreateView, View, ListView
 from django.core.mail import send_mail
 from .forms import UserForm, ProjectForm, SendEmailForm
@@ -6,16 +7,24 @@ from .models import Proyectos, Tags, User
 from portfolio.settings import EMAIL_HOST_USER
 # Create your views here.
 
-class Index(ListView):
-    template_name = "index.html"
-    paginate_by = 6
-    model = Proyectos
-    ordering = ["id"]
+# class Index(ListView):
+#     template_name = "index.html"
+#     paginate_by = 6
+#     model = Proyectos
+#     ordering = ["id"]
 
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        context["tags"] = Tags.objects.all()
-        return context
+#     def get_context_data(self, **kwargs):
+#         context = super().get_context_data(**kwargs)
+#         context["tags"] = Tags.objects.all()
+#         return context
+
+def Index(request):
+    projects_list = Proyectos.objects.all()
+    paginator = Paginator(projects_list, 6)
+
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
+    return render(request, 'index.html', {'page_obj': page_obj, 'tags':Tags.objects.all()})
 
 class RegisterView(CreateView):
     template_name = "registration/register.html"
